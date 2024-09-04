2f6c417e559d2092119042f418f35aa2a585e3f4
Henrique Beserra <millerhiquinho@gmail.com>
2024-09-04 13:47:09 -0300

    Git ignore

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import Cards from './components/molecules/Cards';
 import Header from './components/molecules/Header';
+import Main from './components/molecules/Main';
 import style from './css/Components.module.css';
 
 function App() {
+  
+  let nome_user = window.prompt('Oii, Digite seu nome, por favor!')
+  
   return (
     <div className={style.App}>
-      <Header nome_usuario='Henrique'/>
+      <Header nome_usuario={nome_user}/>
+      <Cards />
+      <Main />
     </div>
   );
 }
