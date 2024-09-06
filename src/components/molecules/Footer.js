d6cc15e869a1f0fe6f1dacdbfb4c8041fb204651
Henrique Beserra <millerhiquinho@gmail.com>
2024-09-06 00:12:22 -0300

    Implementação do elemento footer

---
+++ src/components/molecules/Footer.js
@@ -0,0 +1,10 @@
+import Foot from "../atoms/Foot"
+import style from '../../css/Components.module.css'
+
+export default function Footer () {
+    return (
+        <div className={style.footer}>
+            <Foot />
+        </div>
+    )
+}
