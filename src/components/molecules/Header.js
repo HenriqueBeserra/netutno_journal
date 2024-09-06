179c5a487aa609a82eedc92afae5219161222292
Henrique Beserra <millerhiquinho@gmail.com>
2024-09-06 00:09:23 -0300

    Implementação de um prompt que capta o nome do leitor e apresenta no título Hi!

--- src/components/molecules/Header.js
+++ src/components/molecules/Header.js
@@ -1,11 +1,14 @@
 import style from "../../css/Components.module.css"
 import Hi from "../atoms/Hi"
 import Explore from "../atoms/Explore"
+import Logo from '../atoms/Logo'
+let nome_user = window.prompt('Oii, Digite seu nome, por favor!')
 
-export default function Header ({nome_usuario}) {
+export default function Header () {
     return (
         <div className={style.header_container}>
-            <Hi nome={nome_usuario} />   
+            <Logo />
+            <Hi  nome={nome_user}/>   
             <Explore />
         </div>
     )
